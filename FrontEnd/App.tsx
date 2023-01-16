b0ea5fb1a9df5ff5e7fec1ce4f95227835546ab1
Omarrifky <omarkhaledrifky@hotmail.com>
2023-01-16 22:25:42 +0200

    Added button

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -1,15 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import {  Alert, Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App(){
   return(
-    <View style = {styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style = {styles.container}>
+      <Text>EdSet</Text>
+      <Button
+   onPress={() => Alert.alert('Button with adjusted color pressed')}
+  title="Lets get Started!"
+  accessibilityLabel="Learn more about this purple button"
+/>
+    </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    backgroundColor:"yellow",
     alignItems:"center",
     justifyContent:"center",
   },
