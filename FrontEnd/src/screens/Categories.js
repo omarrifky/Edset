0639b8e445d22b2fdc8b8f0b2fdec49efd2fe28d
Omarrifky <omarkhaledrifky@hotmail.com>
2023-02-21 22:00:02 +0200

    Added Category page

--- FrontEnd/src/screens/Categories.js
+++ FrontEnd/src/screens/Categories.js
@@ -1,9 +1,45 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text,View } from "react-native";
 
 export default function CategoriesScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Categories Screen</Text>
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Architecture Engineering</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>Mechanical Engineering</Text>
+      </View>    
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Mechanical Engineering</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>Computer Engineering</Text>
+      </View> 
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Civil Engineering</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>Pharmacy</Text>
+      </View> 
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Medicine</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>Dentistry</Text>
+      </View> 
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Mechatronics</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>School</Text>
+      </View> 
+      <View style={styles.card1}>
+      <Text style={styles.card1Text} >Applied Arts</Text>
+      </View> 
+      <View style={styles.card2}>
+      <Text  style={styles.card2Text}>Other</Text>
+      </View> 
+     
     </SafeAreaView>
   )
 }
@@ -11,7 +47,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFE605",
-    alignItems: "center",
-    justifyContent: "center",
+
   },
+  card1:{
+    width:"100%",
+    height:"8%",
+    backgroundColor:"white",
+    alignContent:"center",
+    justifyContent:"center"
+  },card2:{
+    width:"100%",
+    height:"8%",
+    backgroundColor:"#FFE605",
+    alignContent:"center",
+    justifyContent:"center"
+  },
+  card1Text:{
+    color: "black",
+    fontWeight: 700,
+    fontSize: 20,
+    alignSelf:"center"
+  },
+  card2Text:{
+    color: "white",
+    fontWeight: 700,
+    fontSize: 20,
+    alignSelf:"center"
+
+  }
+  
 })
