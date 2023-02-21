3178580540adc6ac0da17dbff942045c08909380
Ahmed Samir Morsy <ahmed.samir@tempo.fit>
2023-02-21 20:38:57 +0200

    add icons

--- FrontEnd/src/components/bottomNavbar.js
+++ FrontEnd/src/components/bottomNavbar.js
@@ -3,16 +3,34 @@ import CartScreen from '../screens/Cart';
 import CategoriesScreen from '../screens/Categories';
 import FavoritesScreen from '../screens/Favorites';
 import HomeScreen from '../screens/HomeScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const BottomNavbar = (props) => {
     const Tab = createBottomTabNavigator();
-    
+
     return (
         <Tab.Navigator headerMode="none">
-            <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false, headerMode: false }} />
-            <Tab.Screen name="Favorites" component={FavoritesScreen} options={{ headerShown: false, headerMode: false }} />
-            <Tab.Screen name="Categories" component={CategoriesScreen} options={{ headerShown: false, headerMode: false }} />
-            <Tab.Screen name="Cart" component={CartScreen} options={{ headerShown: false, headerMode: false }} />
+            <Tab.Screen name="Home" component={HomeScreen} options={{
+                headerShown: false, headerMode: false, tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="home" color={color} size={size} />
+                )
+            }} />
+            <Tab.Screen name="Favorites" component={FavoritesScreen} options={{
+                headerShown: false, headerMode: false, tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="heart" color={color} size={size} />
+                )
+            }} />
+            <Tab.Screen name="Categories" component={CategoriesScreen} options={{
+                headerShown: false, headerMode: false, tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="menu" color={color} size={size} />
+                )
+            }} />
+            <Tab.Screen name="Cart" component={CartScreen} options={{
+                headerShown: false, headerMode: false, tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="cart" color={color} size={size} />
+                )
+            }} />
         </Tab.Navigator>
     )
 }
