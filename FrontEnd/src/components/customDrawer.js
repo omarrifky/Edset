f299fcf6456619ff311323bc87d4567199268d0f
Ahmed Samir Morsy <ahmed.samir@tempo.fit>
2023-09-02 18:15:55 +0300

    title case

--- FrontEnd/src/components/customDrawer.js
+++ FrontEnd/src/components/customDrawer.js
@@ -3,6 +3,7 @@ import { View, Button, StyleSheet, SafeAreaView, Image, Text, Pressable } from '
 import { ScrollView } from 'react-native-gesture-handler';
 import { NavigationActions } from 'react-navigation';
 import { AuthContext } from '../providers/auth';
+import { toTitleCase } from '../screens/Categories';
 
 const CustomDrawer = (props) => {
     const { navigation } = props;
@@ -11,7 +12,7 @@ const CustomDrawer = (props) => {
         <SafeAreaView style={styles.safe}>
             <View style={styles.titleHolder}>
                 <Image style={styles.pic} source={{ uri: "https://scontent.fcai2-2.fna.fbcdn.net/v/t1.6435-9/78416817_10218449129812468_5834337807438446592_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RhmGHff4ErYAX_u8Rsd&_nc_ht=scontent.fcai2-2.fna&oh=00_AfBZHofQDA_ct-42lh03bD-loNMK9Lfv5Fq5lUwWKt4vAA&oe=63FE5852" }} />
-                <Text style={[styles.text, styles.title]}>{user.firstname} {user.lastname}</Text>
+                <Text style={[styles.text, styles.title]}>{toTitleCase(user.firstname)} {toTitleCase(user.lastname)}</Text>
             </View>
             <ScrollView style={styles.container}>
                 <View>
