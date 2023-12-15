e67055514018dc8160ac683b4dd65809211af380
omarrifky <omarkhaledrifky@hotmail.com>
2023-12-15 19:52:36 +0200

    Added Supplier and Delivery

---
+++ Supplier-Delivery-Frontend/src/app/pages/delivery/deliveryhomepage/deliveryhomepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliveryhomepage',
+  templateUrl: './deliveryhomepage.component.html',
+  styleUrls: ['./deliveryhomepage.component.scss']
+})
+export class DeliveryhomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Supplier-Delivery-Frontend/src/app/pages/supplier/supplierhomepage/supplierhomepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplierhomepage',
+  templateUrl: './supplierhomepage.component.html',
+  styleUrls: ['./supplierhomepage.component.scss']
+})
+export class SupplierhomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
