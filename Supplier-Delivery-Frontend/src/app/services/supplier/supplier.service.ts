12cc070d2b5f3c68da223bb2b90921688ed56ef1
omarrifky <omarkhaledrifky@hotmail.com>
2023-12-15 21:02:22 +0200

    Added services

---
+++ Supplier-Delivery-Frontend/src/app/services/supplier/supplier.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  baseUrl: string = `${environment.baseUrl}/supplier`;
+  baseorderUrl: string = `${environment.baseUrl}/order`;
+
+  supplier :any;
+
+  constructor(private http: HttpClient) { }
+
+  getSupplier(id: string) {
+    return this.http.get(this.baseUrl+'/viewsupplier/'+id);
+  }
+  getSuppliers(search:any){
+    return this.http.get(this.baseUrl+`/allsuppliers${search ? `?search=${search}`: ''}`);
+  }
+
+
+  createSupplier(){
+    const url = `${this.baseUrl}/register`;
+    return this.http.post(url, {
+  
+    });
+  }
+  updateInfo(id:any ){
+ const url = `${this.baseUrl}/updatemyinfo/`;
+    this.supplier= this.http.get(this.baseUrl+'/viewsupplier/'+id); ;
+
+  
+    return this.http.patch(url, {
+  
+    
+    });
+  }
+  getmyorders(query = null){
+    const url = `${this.baseorderUrl}/myorderssupplier${query ? `?query=${JSON.stringify(query)}` : ''}`;
+    return this.http.get(url)
+  }
+}
